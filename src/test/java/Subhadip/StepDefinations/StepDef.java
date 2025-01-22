7b2930a51cadd65ea8ef081b607edcd31be09213
SubhadipKar-QA <Subha2016@git.com>
2025-01-22 19:52:20 +0530

    Project created

---
+++ src/test/java/Subhadip/StepDefinations/StepDef.java
@@ -0,0 +1,74 @@
+package Subhadip.StepDefinations;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+
+import Subhadip.PageObjects.LandingPage;
+import Subhadip.TestComponents.BaseTest;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import Subhadip.PageObjects.ConfirmationPage;
+import Subhadip.PageObjects.CartPage;
+import Subhadip.PageObjects.CheckoutPage;
+import Subhadip.PageObjects.ProductCatalogue;
+
+
+public class StepDef extends BaseTest {
+	
+	public LandingPage landingPage;
+	public ProductCatalogue productCatalogue;
+	public ConfirmationPage confirmationPage;
+	
+	
+	@Given("I landed on Ecommerce Page")
+	public void I_landed_on_Ecommerce_Page() throws IOException
+	{
+		landingPage = launchApplication();
+		
+	}
+	
+	@Given ("^Logged in with username (.+) and password (.+)$")
+	public void Logged_With_username_password(String username, String password) {
+		productCatalogue = landingPage.loginApplication(username,password);
+	}
+	
+	
+	
+	@When("^I add product (.+) to Cart$")
+	public void i_add_product_to_cart(String productName) throws InterruptedException
+	{
+		//List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+	}
+	
+	
+	
+	@When("^Checkout (.+) and submit the order$")
+	public void checkout_submit_order(String productName)
+	{
+		CartPage cartPage = productCatalogue.goToCartPage();
+
+		Boolean match = cartPage.VerifyProductDisplay(productName);
+		Assert.assertTrue(match);
+		CheckoutPage checkoutPage = cartPage.goToCheckout();
+		checkoutPage.selectCountry("india");
+		confirmationPage = checkoutPage.submitOrder();
+	}
+	
+	
+	@Then("{string} message is displayed on ConfirmationPage")
+    public void message_displayed_confirmationPage(String string)
+    {
+    	String confirmMessage = confirmationPage.getConfirmationMessage();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase(string));
+		driver.close();
+    }
+	
+	
+	
+	
+	
+
+}
